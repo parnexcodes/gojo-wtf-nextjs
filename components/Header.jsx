978018f2f1b2d154dd2c902b610024c0278e4ad2
parnexcodes <parnexcodes@gmail.com>
2022-12-31 09:40:24 +0530

    update logo

--- components/Header.jsx
+++ components/Header.jsx
@@ -9,9 +9,9 @@ function Header() {
     <div className="bg-[#282C37]">
       <div className="flex justify-between mx-8 py-6 flex-wrap">
         <Link href={'/'}>
-        <img className="h-[40px]" src="/logo.png" alt="" />
+        <img className="h-[64px] rounded-full" src="/logo.png" alt="" />
         </Link>
-        <ul className="flex space-x-8 text-white">
+        <ul className="flex space-x-8 text-white py-2">
           <Link href={"/"}>
             <li className="bg-[#2E343E] hover:bg-lime-500 hover:text-black px-4 py-2 rounded-md">Trending</li>
           </Link>
@@ -22,7 +22,7 @@ function Header() {
             <li className="bg-[#2E343E] hover:bg-lime-500 hover:text-black px-4 py-2 rounded-md">Recent</li>
           </Link>
         </ul>
-        <ul className="flex space-x-4 flex-wrap">
+        <ul className="flex space-x-4 flex-wrap py-2">
           <li>
             <Search />
           </li>
